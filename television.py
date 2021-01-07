5518f568edcec070e44a28d794dfec26910b7a44
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-01-08 00:40:27 +0300

    add heroku | TOKEN

--- television.py
+++ television.py
@@ -3,9 +3,8 @@ import telebot
 
 from test2 import OUT_FILENAME
 from telebot import types
-from config import TOKEN
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(token = '1436182600:AAFZQH6fxNO3omQa7r2SRnIGecdA1EkTL6I')
 
 with open(OUT_FILENAME, 'r', encoding="utf-8") as f:
     s = json.load(f)
